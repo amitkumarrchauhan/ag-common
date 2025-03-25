5f13d7895c3d84f3405b37229032f7980817f544
Amitkumar <chauhan.amitkumar@gmail.com>
2025-03-25 16:47:06 +0800

    []: new config added for dist

--- packages/utils/index.js
+++
@@ -1,4 +0,0 @@
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.check = void 0;
-var check_1 = require("./check");
-Object.defineProperty(exports, "check", { enumerable: true, get: function () { return check_1.check; } });
--- packages/utils/rollup.config.js
+++ packages/utils/rollup.config.js
@@ -1,13 +1,39 @@
 import dts from 'rollup-plugin-dts';
 
-// export default {
-//   input: 'dist/index.js',
-//   output: {
-//       file: 'index.js'
-//   },
-//   plugins: [dts()]
-// }
+import { globSync } from 'glob';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
+export default [
+  {
+    input: Object.fromEntries(
+      globSync('dist/**/*.js').map(file => [
+        // This removes `src/` as well as the file extension from each
+        // file, so e.g. src/nested/foo.js becomes nested/foo
+        path.relative(
+          'dist',
+          file.slice(0, file.length - path.extname(file).length)
+        ),
+        // This expands the relative paths to absolute paths, so e.g.
+        // src/nested/foo becomes /project/src/nested/foo.js
+        fileURLToPath(new URL(file, import.meta.url))
+      ])
+    ),
+    output: {
+      format: 'es',
+      dir: 'dist'
+    }
+  },
+  {
+    input: 'dist/index.d.ts',
+    output: {
+        file: 'index.d.ts'
+    },
+    plugins: [dts()]
+  }
+];
+
+/* 
 export default [
   {
     input: 'dist/index.js',
@@ -22,4 +48,4 @@ export default [
       },
       plugins: [dts()]
   }
-]
+] */
