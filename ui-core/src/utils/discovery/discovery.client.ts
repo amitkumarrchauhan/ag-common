399f900d3cf0947f620e42d2ee7dd0b198f6e5f3
Amitkumar Chauhan <chauhan.amitkumar@theoptimum.net>
2025-03-26 19:01:19 +0800

    []: changes done to export module to be used as lib from other ui apps

--- ui-core/src/utils/discovery/discovery.client.ts
+++ ui-core/src/utils/discovery/discovery.client.ts
@@ -1,3 +1,3 @@
 export const check = (name: string): string => {
-  return `Hello ${name}, system is working fine.`
+  return `Hello ${name}, system is working fine, also the local modules hot reload working ... :)`
 }
