0df65d57cf4fcc6af167433d855c934d07c50343
Amitkumar <chauhan.amitkumar@gmail.com>
2025-03-29 10:17:04 +0800

    []: changes done for adding delay so that all watchers start correctly

--- watch-all-servers.bat
+++ watch-all-servers.bat
@@ -1,10 +1,15 @@
 set NODE_ENV=development
 
 start cmd /k "cd core-utils && pnpm run watch:win"
+timeout 5 
 start cmd /k "cd core-utils && pnpm run watch:dt.ts"
+timeout 2
 
 start cmd /k "cd core-utils-svc && pnpm run watch:win"
+timeout 5 
 start cmd /k "cd core-utils-svc && pnpm run watch:dt.ts"
+timeout 2
 
 start cmd /k "cd ui-components && pnpm run watch:win"
+timeout 5 
 start cmd /k "cd ui-components && pnpm run watch:dt.ts"
